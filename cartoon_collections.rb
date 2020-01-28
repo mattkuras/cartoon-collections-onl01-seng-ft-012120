bbfdb957944887a2a65da3c41ad91479d8cd95d8
Matt Kuras <mattkuras@gmail.com>
2020-01-28 22:22:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? {|element| 
+  array.any? {|element| element.length > 4
 end
 
 def find_the_cheese# code an argument here
