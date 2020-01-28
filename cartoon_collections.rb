6863159b0676499175649d072da9b3436611b57a
Matt Kuras <mattkuras@gmail.com>
2020-01-28 22:42:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.detect{|cheese| cheese.any? ("cheddar", "gouda", "camembert")}
+  array.detect{|cheese| cheese.any?("cheddar", "gouda", "camembert")}
 end
